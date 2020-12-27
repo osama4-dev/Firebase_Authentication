8c494ea69a1c6d303d638373907dcebfa4d0b4f6
Yasir Hussain <yasir9001@yahoo.com>
2020-12-27 02:46:10 -0800

    Fixed Login UI

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,13 +6,18 @@ import NavigationItem from '../NavigationItem/NavigationItem';
 const navigationItems = (props) => (
     <ul className="NavigationItems">
        
-      
+        <div className="Logo">
+            <p>Logo</p>
+        </div>
+        <div className="Auth">
         {!props.isAuthenticated
             ? <NavigationItem link="/SignUp">SignUp</NavigationItem>
             : <NavigationItem link="/SignIn">SignIn</NavigationItem>}
              {!props.isAuthenticated
             ? <NavigationItem link="/SignIn">SignIn</NavigationItem>
             : <NavigationItem link="/SignUp">SignUp</NavigationItem>}
+        </div>
+        
     </ul>
 );
 
